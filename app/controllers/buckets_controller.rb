65134ed326cf18c7dab2ea44a670478de8138735
Taylor Wu <Taylorwu21@gmail.com>
2016-06-02 08:56:14 -0600

    added buckets controller

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,13 +1,52 @@
 class BucketsController < ApplicationController
-  def index
-  end
+	before_action :bucket, except: [:index, :new, :create]
 
-  def new
+  def index
+  	@buckets = current_user.buckets
   end
 
   def show
   end
 
+  def new
+  	@bucket = Bucket.new
+  end
+
+  def create
+  	@bucket = Bucket.new(bucket_params)
+  	if @bucket.save
+  		flash[:success] = "Bucket List Created."
+  		redirect_to buckets_path
+  	else
+  		flash[:notice] = "Failed to create"
+  		render :new
+  	end
+  end
+
   def edit
   end
+
+  def update
+  	if @bucket.update(bucket_params)
+  		flash[:success] = "Update completed"
+  		redirect_to bucket_path
+  	else
+  		flash[:notice] = "Failed to update bucket list."
+  		render :edit
+  end
+
+  def destroy
+  	@bucket.destroy
+  	redirect_to buckets_path
+  end
+
+  private
+
+  	def bucket_params
+  		params.require(:bucket).permit(:title, :description, :completed)
+  	end
+
+  	def bucket
+  		@bucket = current_user.buckets.find(params[:id])
+  	end
 end
