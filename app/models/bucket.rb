6833a504a820980f42c80a619b89dd6e458879ab
Faith Oladele <faitholadele@gmail.com>
2016-06-02 20:39:04 -0600

    add devise view

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -2,7 +2,7 @@ class Bucket < ActiveRecord::Base
   belongs_to :user
   has_many :lists, dependent: :destroy
   
-  has_attached_file :image, styles: { large: "600x600",medium: "300x300>", thumb: "100x100>" }
+  has_attached_file :image, styles: { large: "600x600", medium: "300x300#", thumb: "100x100>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def self.search(search)
